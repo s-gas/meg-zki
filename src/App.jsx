8a77475797729fbf134fca5fc1b385ad9363cea2
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-06-19 11:26:55 +0200

    Add awards section and create Link component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Main from './components/Main'
+import Sections from './components/Sections'
 
 function App() {
   return (
     <>
       <Navbar />
       <Main />
+      <Sections />
     </>
   )
 }
---
+++ src/components/Sections.jsx
@@ -0,0 +1,75 @@
+const About = () => {
+  return (
+    <div className="section" id="about">
+      <p className="title">(ABOUT)</p>
+      <p className="content">I’m Meg 👋 an independent digital designer driven by a concept-first approach when developing brand identities and crafting meaningful experiences. Originally from the UK and now residing in Berlin. Previously at <Link text="Fiasco Design" link="https://fiasco.design/" /> and  <Link text="true" link="https://www.truedigital.co.uk/" /></p>
+      <p className="content">Awarded a Master’s in Visual & Experience Design from the University of Europe for Applied Sciences under the direction of <Link text="Prof. Dr. Peter Crnokrak" link="https://theluxuryofprotest.com/" />.</p>
+      <p className="content">Lately, I’ve been exploring the world of data-driven, generative, and interactive installation design.</p>
+      <p className="footer">Get in touch for full portfolio and availability.</p>
+    </div>
+  )
+}
+
+const Link = (props) => {
+  const {text, link} = props;
+  return (
+    <a href={link} target="_blank" rel="noopener noreferrer" className="external-link">{text}</a>
+  )
+}
+
+const Awards = () => {
+  const awards = [
+    {
+      text: "BIMA Awards 2022: Aardman",
+      link: "https://bima.co.uk/aardman-com-where-ux-meets-wonk-true/",
+    },
+    {
+      text: "BIMA Awards 2022: UK Sport",
+      link: "https://bima.co.uk/uk-sport-levelling-the-playing-field-in-elite-sport-true/",
+    },
+    {
+      text: "Lovie Awards 2022 (Silver)",
+      link: "https://winners.lovieawards.com/search/?sw=aardman&sort=&pg=1#w233749",
+    },
+    {
+      text: "The Webby Awards 2022 (Honoree)",
+      link: "https://winners.webbyawards.com/2022/websites-and-mobile-sites/general-desktop-mobile-sites/corporate-communications/213808/aardmancom--full-of-character",
+    },
+    {
+      text: "CSS Design Awards 2022 (SOTD)",
+      link: "https://www.cssdesignawards.com/sites/aardman/41131/",
+    },
+    {
+      text: "Sports Business Awards 2022 (Bronze)",
+      link: "https://sportsbusinessawards.co.uk/winners-2022/",
+    },
+    {
+      text: "Awwwards 2022 (Honourable Mention)",
+      link: "https://www.awwwards.com/sites/aardman",
+    },
+    {
+      text: "The Drum Awards 2021 (Winner)",
+      link: "https://www.thedrum.com/awards-case-study/how-uk-sport-encouraging-more-young-people-diverse-backgrounds-become-olympians",
+    },
+  ];
+  return (
+    <div className="section" id="awards">
+      <p className="title">(AWARDS)</p>
+      { awards.map((award) => (
+        <Link key={award.link} text={award.text} link={award.link} />
+      ))}
+      <p className="footer">All awards were earned during my time  working in-house and represent the collective achievements of a whole team (˶ᵔ ᵕ ᵔ˶)</p>
+    </div>
+  )
+}
+
+const Sections = () => {
+  return (
+    <div className="sections">
+      <About />
+      <Awards />
+    </div>
+  )
+}
+
+export default Sections
