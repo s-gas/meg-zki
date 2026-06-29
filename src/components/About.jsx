89c04cd3167877edeb07280c04b2f4eaca8c97cf
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-06-29 15:55:44 +0200

    Add image on hover

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,11 @@
 import Link from './Link'
+import Name from './Name'
 
 const About = () => {
   return (
     <div className="section" id="about">
       <p className="section-title">(ABOUT)</p>
-      <p className="section-content">I’m Meg 👋 an independent digital designer driven by a concept-first approach when developing brand identities and crafting meaningful experiences. Originally from the UK and now residing in Berlin. Previously at <Link text="Fiasco Design" link="https://fiasco.design/" /> and  <Link text="true" link="https://www.truedigital.co.uk/" />.</p>
+      <p className="section-content">I’m <Name /> 👋 an independent digital designer driven by a concept-first approach when developing brand identities and crafting meaningful experiences. Originally from the UK and now residing in Berlin. Previously at <Link text="Fiasco Design" link="https://fiasco.design/" /> and  <Link text="true" link="https://www.truedigital.co.uk/" />.</p>
       <p className="content">Awarded a Master’s in Visual & Experience Design from the University of Europe for Applied Sciences under the direction of <Link text="Prof. Dr. Peter Crnokrak" link="https://theluxuryofprotest.com/" />.</p>
       <p className="section-content">Lately, I’ve been exploring the world of data-driven, generative, and interactive installation design.</p>
       <p className="section-footer">Get in touch for full portfolio and availability.</p>
---
+++ src/components/Name.jsx
@@ -0,0 +1,12 @@
+import './Name.css'
+
+const Name = () => {
+  return (
+    <span className="name-hover">
+      Meg
+      <img className="image-hover" src="/images/mk.jpg" alt="" />
+    </span>
+  )
+}
+
+export default Name
