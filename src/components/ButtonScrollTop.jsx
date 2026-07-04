a187c83a3a2723f598566e680b34f49ff51503c5
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-07-04 13:43:32 +0200

    Add hover effect for back to top button

--- src/components/ButtonScrollTop.jsx
+++ src/components/ButtonScrollTop.jsx
@@ -1,8 +1,8 @@
 import backToTop from '../utils/backToTop'
 
-const ButtonScrollTop = () => {
+const ButtonScrollTop = (props) => {
   return (
-    <button className="cursor-pointer" onClick={() => backToTop("smooth")}>
+    <button className="cursor-pointer" onClick={() => backToTop("smooth")} {...props}>
       <svg className="hover:text-ink-hover" width="18" height="26" viewBox="0 0 18 26" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M8.73145 1.46558V25.4656M16.7314 10.037L8.73145 1.46558L0.731445 10.037" stroke="currentColor" strokeWidth="2"/>
       </svg>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,17 @@
+import { useState } from 'react'
 import ButtonScrollTop from './ButtonScrollTop'
 
 const Footer = () => {
+  const [opacity, setOpacity] = useState(0);
+
+  const opacityLevel = `opacity-${opacity}`;
+
   return (
     <div className="flex justify-between mt-30 items-end">
       <p className="text-xs text-ink/60">© Meg Krawielitzki 2026</p>
       <div className="flex items-end gap-2">
-        <p className="font-tiempos italic font-light text-sm text-ink/60">Back to top</p>
-        <ButtonScrollTop />
+        <p className={`font-tiempos italic font-light text-sm text-ink/60 ${opacityLevel}`}>Back to top</p>
+        <ButtonScrollTop onMouseEnter={() => setOpacity(100)} onMouseLeave={() => setOpacity(0)}/>
       </div>
     </div>
   )
