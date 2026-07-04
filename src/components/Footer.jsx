7084b827058db40669c2ebda29a99603d8576271
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-07-04 18:36:14 +0200

    Generate year from Date

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,11 @@ const Footer = () => {
 
   const opacityLevel = `opacity-${opacity}`;
 
+  const year = new Date().getFullYear();
+
   return (
     <div className="flex justify-between mt-30 items-end">
-      <p className="text-xs text-ink/60">© Meg Krawielitzki 2026</p>
+      <p className="text-xs text-ink/60">{`© Meg Krawielitzki ${year}`}</p>
       <div className="flex items-end gap-2">
         <p className={`font-tiempos italic font-light text-sm text-ink/60 ${opacityLevel}`}>Back to top</p>
         <ButtonScrollTop onMouseEnter={() => setOpacity(100)} onMouseLeave={() => setOpacity(0)}/>
