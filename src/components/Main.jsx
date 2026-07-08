75ee2b0d20869cc453fa37b9c0a3b66490869ae4
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-07-08 21:44:33 +0200

    Increase minimum text size of hero and top margin for mobile

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,9 +9,9 @@ const Main = () => {
   });
 
   return (
-    <main className={`mt-20 sm:mt-90 transition-all duration-2000 ${visible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'}`}>
-      <p className="text-[clamp(1rem,5.5vw,20rem)] font-medium leading-none text-stroke-xs sm:text-stroke-s md:text-stroke-m lg:text-stroke-l">Berlin-based digital designer creating <SerifItalic>human-centred</SerifItalic> experiences that blend <SerifItalic>form</SerifItalic> and <SerifItalic>functionality</SerifItalic>.</p>
-      <p className="indent-[clamp(1rem,7vw,20rem)] text-[clamp(1rem,5.5vw,20rem)] font-medium leading-none text-stroke-xs sm:text-stroke-s md:text-stroke-m lg:text-stroke-l">With expertise in <SerifItalic>creative concepting</SerifItalic>, <SerifItalic>user interface design</SerifItalic>, and <SerifItalic>visual identities</SerifItalic>, taking ideas from concept right through to production.</p>
+    <main className={`mt-30 sm:mt-90 transition-all duration-2000 ${visible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'}`}>
+      <p className="text-[clamp(1.6rem,5.5vw,20rem)] font-medium leading-none text-stroke-xs sm:text-stroke-s md:text-stroke-m lg:text-stroke-l">Berlin-based digital designer creating <SerifItalic>human-centred</SerifItalic> experiences that blend <SerifItalic>form</SerifItalic> and <SerifItalic>functionality</SerifItalic>.</p>
+      <p className="indent-[clamp(1.6rem,7vw,20rem)] text-[clamp(1.6rem,5.5vw,20rem)] font-medium leading-none text-stroke-xs sm:text-stroke-s md:text-stroke-m lg:text-stroke-l">With expertise in <SerifItalic>creative concepting</SerifItalic>, <SerifItalic>user interface design</SerifItalic>, and <SerifItalic>visual identities</SerifItalic>, taking ideas from concept right through to production.</p>
     </main>
   )
 }
