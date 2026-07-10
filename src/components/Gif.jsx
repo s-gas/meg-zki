b35f8bc23e38343ed459f40cc31a1b8bf32ae139
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-07-10 10:09:32 +0200

    Make GIF and portrait images more accessible

--- src/components/Gif.jsx
+++ src/components/Gif.jsx
@@ -1,7 +1,7 @@
 const Gif = ({visible}) => {
   return (
-    <div className={`mt-40 sm:mt-0 sm:fixed sm:inset-0 sm:z-10 ${visible ? "sm:opacity-100" : "sm:opacity-0 sm:pointer-events-none"} sm:flex sm:items-center sm:justify-center`}>
-      <img src="/images/mk.gif" className="sm:w-1/2"/>
+    <div className={`mt-40 sm:mt-0 sm:fixed sm:inset-0 sm:z-10 ${visible ? "sm:flex" : "sm:hidden"} sm:flex sm:items-center sm:justify-center`}>
+      <img src="/images/mk.gif" className="sm:w-1/2" alt="mockups of past projects"/>
     </div>
   )
 }
--- src/components/Portrait.jsx
+++ src/components/Portrait.jsx
@@ -1,6 +1,10 @@
 const Portrait = ({visible}) => {
   return (
-      <img className={`sm:absolute sm:-top-20 sm:left-28 sm:max-w-none sm:w-70 ${visible ? "sm:opacity-100" : "sm:opacity-0 sm:pointer-events-none"}`} src="/images/mk.jpg" alt="" />
+      <img
+        className={`sm:absolute sm:-top-20 sm:left-28 sm:max-w-none sm:w-70 ${visible ? "sm:flex" : "sm:hidden"}`}
+        src="/images/mk.jpg" 
+        alt="Portrait of Meg Krawielitzki" 
+      />
   )
 }
 
